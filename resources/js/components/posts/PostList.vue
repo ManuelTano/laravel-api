b54de9170203231ce7c3670807ae471b171a9d6a
ManuelTano <manueltano98@gmail.com>
2022-10-04 17:06:43 +0200

    BONUS: Loader & OrderBy

--- resources/js/components/posts/PostList.vue
+++ resources/js/components/posts/PostList.vue
@@ -1,42 +1,51 @@
 <template>
-    <section id="posts-list">
-        <h2 class="my-3">Posts</h2>
-        <div></div>
-        <div v-if="posts.length">
-            <PostCard v-for="post in posts" :key="post.id" :post="post" />
-        </div>
-        <h5 v-else>Non ci sono posts</h5>
-    </section>
+  <section id="posts-list">
+    <h2 class="my-3">Posts</h2>
+
+    <!-- AppLoader -->
+    <AppLoader v-if="isLoading" />
+
+    <div v-else>
+      <div v-if="posts.length">
+        <PostCard v-for="post in posts" :key="post.id" :post="post" />
+      </div>
+      <h5 v-else>Non ci sono posts</h5>
+    </div>
+  </section>
 </template>
 
 <script>
-import PostCard from "./PostCard.vue"
+import AppLoader from "../AppLoader.vue";
+import PostCard from "./PostCard.vue";
 export default {
-    name: "PostsList",
-    data() {
-        return {
-            posts: [],
-        };
+  name: "PostsList",
+  data() {
+    return {
+      posts: [],
+      isLoading: false,
+    };
+  },
+  components: { PostCard, AppLoader },
+  methods: {
+    fetchPosts() {
+      this.isLoading = true;
+      axios
+        .get("http://localhost:8000/api/posts")
+        .then((res) => {
+          this.posts = res.data;
+        })
+        .catch((err) => {
+          console.error(err);
+        })
+        .then(() => {
+          this.isLoading = false;
+          console.log("Chiamata terminata");
+        });
     },
-    components:{PostCard},
-    methods: {
-        fetchPosts() {
-            axios
-                .get("http://localhost:8000/api/posts")
-                .then((res) => {
-                    this.posts = res.data;
-                })
-                .catch((err) => {
-                    console.error(err);
-                })
-                .then(() => {
-                    console.log("Chiamata terminata");
-                });
-        },
-    },
-    mounted() {
-        this.fetchPosts();
-    },
+  },
+  mounted() {
+    this.fetchPosts();
+  },
 };
 </script>
 <style scoped lang="scss"></style>
